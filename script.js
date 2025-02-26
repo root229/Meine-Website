277c16b29aeff3e9f735eeef89907b287a811b68
root229 <122137338+root229@users.noreply.github.com>
2025-02-26 13:15:15 +0100

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Elemente auswählen
-const popup = document.getElementById("popup");
-const infoButton = document.getElementById("infoButton");
-const closePopup = document.getElementById("closePopup");
-
-// Event-Listener für den Info-Button
-infoButton.addEventListener("click", () => {
-  popup.style.display = "flex"; // Pop-up sichtbar machen
-});
-
-// Event-Listener für das Schließen
-closePopup.addEventListener("click", () => {
-  popup.style.display = "none"; // Pop-up ausblenden
-});
-
-// Schließen des Pop-ups bei Klick außerhalb des Inhalts
-window.addEventListener("click", (event) => {
-  if (event.target === popup) {
-    popup.style.display = "none";
-  }
-});
 window.onload = function () {
   const minWidth = 678; // Mindestbreite in Pixel
   const minHeight = 940; // Mindesthöhe in Pixel
@@ -41,4 +20,3 @@ window.onload = function () {
     document.body.prepend(warning);
   }
 };
-console.log("Breite:", window.innerWidth, "Höhe:", window.innerHeight);
